90421d2bcadcacb3ef9f1ee888e011dc93d94dd6
hqmq <michael@riesd.com>
2013-12-12 15:35:01 -0700

    changing #render to #render_attribute
    this avoids a naming conflict with rails view specs

--- lib/rspec/rabl/matchers.rb
+++ lib/rspec/rabl/matchers.rb
@@ -1,7 +1,7 @@
 module RSpec
   module Rabl
     module Matchers
-      def render(attribute)
+      def render_attribute(attribute)
         AttributeMatcher.new(attribute, _rabl_opts)
       end
     end
--- spec/functional/custom_matchers_spec.rb
+++ spec/functional/custom_matchers_spec.rb
@@ -5,42 +5,42 @@ describe "Customer Matchers" do
 
   describe "user.rabl" do
     rabl_data(:root => 'user'){ user }
-    it{ should render(:guid) }
-    it{ should render(:first_name) }
-    it{ should render(:last_name) }
-    it{ should render(:email) }
-    it{ should_not render(:password) }
+    it{ should render_attribute(:guid) }
+    it{ should render_attribute(:first_name) }
+    it{ should render_attribute(:last_name) }
+    it{ should render_attribute(:email) }
+    it{ should_not render_attribute(:password) }
   end
 
   describe "user_aliases.rabl" do
     rabl_data(:root => 'user'){ user }
-    it{ should render(:id).with(:guid) }
-    it{ should render(:team).with_value('Gorby Puff') }
+    it{ should render_attribute(:id).with(:guid) }
+    it{ should render_attribute(:team).with_value('Gorby Puff') }
   end
 
   describe "index.rabl" do
     rabl_data(:root => 'users', :object_root => 'user'){ [user] }
-    it{ should render(:guid) }
-    it{ should render(:first_name) }
-    it{ should render(:last_name) }
-    it{ should render(:email) }
-    it{ should_not render(:password) }
+    it{ should render_attribute(:guid) }
+    it{ should render_attribute(:first_name) }
+    it{ should render_attribute(:last_name) }
+    it{ should render_attribute(:email) }
+    it{ should_not render_attribute(:password) }
   end
 
   describe "rootless_index.rabl" do
     rabl_data(:root => false, :object_root => 'user'){ [user] }
-    it{ should render(:guid) }
-    it{ should_not render(:password) }
+    it{ should render_attribute(:guid) }
+    it{ should_not render_attribute(:password) }
   end
 
   describe "bare_index.rabl" do
     rabl_data(:root => false, :object_root => false){ [user] }
-    it{ should render(:guid) }
-    it{ should_not render(:password) }
+    it{ should render_attribute(:guid) }
+    it{ should_not render_attribute(:password) }
   end
 
   describe "nested/test.rabl" do
     rabl_data(:root => 'test'){ user }
-    it{ should render(:guid) }
+    it{ should render_attribute(:guid) }
   end
 end
