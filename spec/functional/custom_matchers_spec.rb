7d0b9f549abf727e2956c3f9df2f8c088201fab9
Michael Ries <michael.ries@moneydesktop.com>
2014-10-23 15:53:18 -0600

    upgrade to the new rspec expect syntax

--- spec/functional/custom_matchers_spec.rb
+++ spec/functional/custom_matchers_spec.rb
@@ -5,60 +5,60 @@ describe "Customer Matchers" do
 
   describe "user.rabl" do
     rabl_data(:root => 'user'){ user }
-    it{ should render_attribute(:guid) }
-    it{ should render_attribute(:first_name) }
-    it{ should render_attribute(:last_name) }
-    it{ should render_attribute(:email) }
-    it{ should_not render_attribute(:password) }
+    it{ expect(subject).to render_attribute(:guid) }
+    it{ expect(subject).to render_attribute(:first_name) }
+    it{ expect(subject).to render_attribute(:last_name) }
+    it{ expect(subject).to render_attribute(:email) }
+    it{ expect(subject).to_not render_attribute(:password) }
   end
 
   describe "user_aliases.rabl" do
     rabl_data(:root => 'user'){ user }
-    it{ should render_attribute(:id).with(:guid) }
-    it{ should render_attribute(:team).with_value('Gorby Puff') }
+    it{ expect(subject).to render_attribute(:id).with(:guid) }
+    it{ expect(subject).to render_attribute(:team).with_value('Gorby Puff') }
   end
 
   describe "index.rabl" do
     rabl_data(:root => 'users', :object_root => 'user'){ [user] }
-    it{ should render_attribute(:guid) }
-    it{ should render_attribute(:first_name) }
-    it{ should render_attribute(:last_name) }
-    it{ should render_attribute(:email) }
-    it{ should_not render_attribute(:password) }
+    it{ expect(subject).to render_attribute(:guid) }
+    it{ expect(subject).to render_attribute(:first_name) }
+    it{ expect(subject).to render_attribute(:last_name) }
+    it{ expect(subject).to render_attribute(:email) }
+    it{ expect(subject).to_not render_attribute(:password) }
   end
 
   describe "index.rabl" do
     rabl_data(:root => 'foobar'){ [user] }
-    it "should give an error specific for missing root" do
+    it "gives an error specific for missing root" do
       expect{
-        subject.should render_attribute(:guid)
+        expect(subject).to render_attribute(:guid)
       }.to raise_error(RSpec::Rabl::Error, /missing root/i)
     end
   end
 
   describe "index.rabl" do
     rabl_data(:root => 'users', :object_root => "player"){ [user] }
-    it "should give an error specific for incorrect object_root" do
+    it "gives an error specific for incorrect object_root" do
       expect{
-        subject.should render_attribute(:guid)
+        expect(subject).to render_attribute(:guid)
       }.to raise_error(RSpec::Rabl::Error, /missing object root/i)
     end
   end
 
   describe "rootless_index.rabl" do
     rabl_data(:root => false, :object_root => 'user'){ [user] }
-    it{ should render_attribute(:guid) }
-    it{ should_not render_attribute(:password) }
+    it{ expect(subject).to render_attribute(:guid) }
+    it{ expect(subject).to_not render_attribute(:password) }
   end
 
   describe "bare_index.rabl" do
     rabl_data(:root => false, :object_root => false){ [user] }
-    it{ should render_attribute(:guid) }
-    it{ should_not render_attribute(:password) }
+    it{ expect(subject).to render_attribute(:guid) }
+    it{ expect(subject).to_not render_attribute(:password) }
   end
 
   describe "nested/test.rabl" do
     rabl_data(:root => 'test'){ user }
-    it{ should render_attribute(:guid) }
+    it{ expect(subject).to render_attribute(:guid) }
   end
 end
