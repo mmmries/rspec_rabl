f647faf6a0301548f8d9634fab1380aa8bd31bda
Zach Toolson <zach.toolson@mx.com>
2016-03-30 10:02:18 -0600

    add nil value check to matcher errors spec

--- spec/functional/matcher_errors_spec.rb
+++ spec/functional/matcher_errors_spec.rb
@@ -45,4 +45,23 @@ describe "Error Messages" do
     expect(lines).to include("  expected \"Imma derp derp\" in [\"users\"][\"user\"][\"guid\"]")
     expect(lines).to include("    got \"abc\"")
   end
+
+  it "renders a helpful error message for nil tests" do
+    example = nil
+    group = RSpec.describe "index.rabl" do
+      include_context "user_context"
+      rabl_data(:root => "users", :object_root => "user"){ [user] }
+      example = it "tries to render nil attribute" do 
+        allow(user).to receive(:foo).and_return(nil)
+        expect(subject).to render_attribute(:foo)
+      end
+    end
+    group.run
+    lines = RSpec::Core::Notifications::FailedExampleNotification.new(example).message_lines.join("\n")
+
+    expect(lines).to include("nil value when testing.")
+    expect(lines).to include("  expected nil in [\"users\"][\"user\"][\"foo\"]")
+    expect(lines).to include("  got nil")
+    expect(lines).to include("  If you want to test for a nil value, please use `with_value(nil)`")
+  end
 end
